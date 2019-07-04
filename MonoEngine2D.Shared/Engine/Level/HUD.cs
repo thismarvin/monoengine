a49133d2dfb04e62a74f2bf90b64fcabf3d789f6
thismarvin <bend_@outlook.com>
2019-07-04 17:41:55 -0500

    made use of new Transition class

--- MonoEngine2D.Shared/Engine/Level/HUD.cs
+++ MonoEngine2D.Shared/Engine/Level/HUD.cs
@@ -4,12 +4,13 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoEngine2D.Engine.Utilities.Cameras;
 using MonoEngine2D.Engine.Utilities.Transitions;
+using MonoEngine2D.Shared.Engine.Utilities.Transitions;
 
 namespace MonoEngine2D.Engine.Level
 {
     static class HUD
     {
-        static Pinhole pinhole;
+        static Transition transition;
 
         static public void Initialize()
         {
@@ -18,19 +19,20 @@ namespace MonoEngine2D.Engine.Level
 
         public static void Reset()
         {
-            pinhole = new Pinhole(Camera.ScreenBounds.Width / 2, Camera.ScreenBounds.Height / 2, Pinhole.Type.Open);
+            transition = new Fade(TransitionType.Exit);
+            transition.Start();
         }
 
         public static void Update(GameTime gameTime)
         {
-            pinhole.Update(gameTime);
+            transition.Update(gameTime);
         }
 
         public static void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, SamplerState.PointClamp, null, null, null, StaticCamera.Transform);
             {
-                pinhole.Draw(spriteBatch);
+                transition.Draw(spriteBatch);
             }
             spriteBatch.End();
         }
