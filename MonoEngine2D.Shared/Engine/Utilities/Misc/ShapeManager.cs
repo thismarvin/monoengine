8fff87c307c115f1c68009f223cdb13e66c74f66
thismarvin <bend_@outlook.com>
2019-07-08 00:03:02 -0500

    added a VertexBuffer to hypothetically improve performance

--- MonoEngine2D.Shared/Engine/Utilities/Misc/ShapeManager.cs
+++ MonoEngine2D.Shared/Engine/Utilities/Misc/ShapeManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -8,20 +9,54 @@ namespace MonoEngine2D.Engine.Utilities.Misc
     {
         public static Texture2D Texture { get; private set; }
 
+        public static VertexBuffer Buffer;
+        private static List<VertexPositionColor> vertices;
+
         private ShapeManager()
         {
-            
+
         }
 
         public static void Initialize(GraphicsDeviceManager graphics)
         {
             Texture = new Texture2D(graphics.GraphicsDevice, 1, 1);
             Texture.SetData(new[] { Color.White });
+
+            vertices = new List<VertexPositionColor>();
+            Buffer = new VertexBuffer(graphics.GraphicsDevice, VertexPositionColor.VertexDeclaration, 0, BufferUsage.WriteOnly);
+        }
+
+        public static void AddToVertexBuffer(VertexPositionColor[] vertices)
+        {
+            foreach (VertexPositionColor vertexPositionColor in vertices)
+            {
+                if (!ShapeManager.vertices.Contains(vertexPositionColor))
+                {
+                    ShapeManager.vertices.Add(vertexPositionColor);
+                }                   
+            }
+        }
+
+        public static void RemoveFromVertexBuffer(VertexPositionColor[] vertices)
+        {
+            for (int i = vertices.Length - 1; i >= 0; i--)
+            {
+                if (ShapeManager.vertices.Contains(vertices[i]))
+                {
+                    ShapeManager.vertices.RemoveAt(i);
+                }
+            }
         }
 
         public static void UnloadContent()
         {
             Texture.Dispose();
+        }
+
+        public static void Update(GraphicsDevice graphicsDevice)
+        {
+            graphicsDevice.SetVertexBuffer(Buffer);
+            Console.WriteLine(vertices.Count);
         }
     }
 }
