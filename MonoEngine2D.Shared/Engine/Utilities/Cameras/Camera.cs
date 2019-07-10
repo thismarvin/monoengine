ea55531a567e0c6e3b544fa834e98a994cf8e128
thismarvin <bend_@outlook.com>
2019-07-10 13:33:33 -0500

    complete camera rework

--- MonoEngine2D.Shared/Engine/Utilities/Cameras/Camera.cs
+++ MonoEngine2D.Shared/Engine/Utilities/Cameras/Camera.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using MonoEngine2D.Engine.Root;
 using MonoEngine2D.Engine.Utilities.Display;
+using MonoEngine2D.Engine.Utilities.User_Input;
 
 namespace MonoEngine2D.Engine.Utilities.Cameras
 {
@@ -15,7 +16,14 @@ namespace MonoEngine2D.Engine.Utilities.Cameras
 
         public static Rectangle Bounds { get; private set; }
 
-        public static Vector2 TopLeft;
+        public static Vector3 TopLeft;
+
+        static Vector3 cameraPosition;
+        static Vector3 cameraTarget;
+        static Vector3 cameraCenter;
+
+        static float rotation;
+
         public static int Scale { get; private set; }
         public static float Zoom { get; private set; }
 
@@ -26,8 +34,12 @@ namespace MonoEngine2D.Engine.Utilities.Cameras
         {
             CreatePixelScene(pixelWidth, pixelHeight, scale);
             Reset(ScreenManager.DefaultWindowWidth, ScreenManager.DefaultWindowHeight);
-            SetupMatrices();
+
+            //rotation = (float)Math.PI / 2;
+
             UpdateMatrices();
+
+            
         }
 
         private static void CreatePixelScene(int pixelWidth, int pixelHeight, int scale)
@@ -35,13 +47,6 @@ namespace MonoEngine2D.Engine.Utilities.Cameras
             Scale = scale;
             Camera.pixelWidth = pixelWidth * Scale;
             Camera.pixelHeight = pixelHeight * Scale;
-        }
-
-        private static void SetupMatrices()
-        {
-            World = Matrix.Identity * Matrix.CreateTranslation(new Vector3(ScreenManager.CurrentWindowWidth / Zoom / 2 - StaticCamera.HorizontalLetterBox + TopLeft.X, ScreenManager.CurrentWindowHeight / Zoom / 2 - StaticCamera.VerticalLetterBox + TopLeft.Y, 0));
-            View = Matrix.CreateLookAt(new Vector3(0, 0, -2), Vector3.Forward, Vector3.Up);
-            Projection = Matrix.CreateOrthographic(ScreenManager.CurrentWindowWidth / Zoom, ScreenManager.CurrentWindowHeight / Zoom, -1000, 1000);
         }
 
         public static void Reset(int windowWidth, int windowHeight)
@@ -85,20 +90,20 @@ namespace MonoEngine2D.Engine.Utilities.Cameras
 
         public static void Update()
         {
-            Input();
+            Inputtt();
             UpdateMatrices();
         }
 
         public static void Update(Vector2 topLeft, float minWidth, float maxWidth, float minHeight, float maxHeight)
         {
-            TopLeft = topLeft * Scale;
+            TopLeft = new Vector3(topLeft.X, topLeft.Y, 0);
 
-            Input();
+            Inputtt();
             StayWithinBounds(minWidth, maxWidth, minHeight, maxHeight);
             UpdateMatrices();
         }
 
-        private static void Input()
+        private static void Inputtt()
         {
             if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
             {
@@ -113,17 +118,42 @@ namespace MonoEngine2D.Engine.Utilities.Cameras
 
         private static void StayWithinBounds(float minWidth, float maxWidth, float minHeight, float maxHeight)
         {
-            // Still Buggy!
             TopLeft.X = (TopLeft.X < minWidth ? minWidth : TopLeft.X);
             TopLeft.X = (TopLeft.X + Bounds.Width > maxWidth ? maxWidth - Bounds.Width: TopLeft.X);
             TopLeft.Y = (TopLeft.Y < minHeight ? 0 : TopLeft.Y);
             TopLeft.Y = (TopLeft.Y + Bounds.Height > maxHeight ? maxHeight - Bounds.Height : TopLeft.Y);
         }
 
+        /// TODO:
+        /// Scale breaks the camera!
+        /// Maybe make it so the camera can zoom in on any given point? 
         private static void UpdateMatrices()
         {
-            Transform = Matrix.CreateTranslation(new Vector3(-TopLeft.X + StaticCamera.HorizontalLetterBox, -TopLeft.Y + StaticCamera.VerticalLetterBox, 0)) * Matrix.CreateScale(new Vector3(Zoom, Zoom, 0));
-            World = Matrix.Identity * Matrix.CreateTranslation(new Vector3(ScreenManager.CurrentWindowWidth / Zoom / 2 - StaticCamera.HorizontalLetterBox + TopLeft.X, ScreenManager.CurrentWindowHeight / Zoom / 2 - StaticCamera.VerticalLetterBox + TopLeft.Y, 0));
+            cameraPosition = new Vector3(-TopLeft.X, -TopLeft.Y, -1);
+            cameraTarget = new Vector3(cameraPosition.X, cameraPosition.Y, 0);
+            cameraCenter = new Vector3(Bounds.Width / 2, Bounds.Height / 2, 0);
+
+            Transform =
+                    // M = R * T * S 
+                    // Translate the transform matrix to the inverse of the camera's center.
+                    Matrix.CreateTranslation(-cameraCenter) *
+                    // Rotate the camera relative to the center of the camera bounds.
+                    Matrix.CreateRotationZ(rotation) *
+                    // Translate the transform matrix to the transform matrix to the inverse of the camera's top left.
+                    Matrix.CreateTranslation(-TopLeft) *
+                    // Scale the transform matrix by the camera's zoom.
+                    Matrix.CreateScale(Zoom) *
+                    // Anchor the transform matrix to the center of the screen instead of the top left.
+                    Matrix.CreateTranslation(new Vector3(ScreenManager.CurrentWindowWidth / 2, ScreenManager.CurrentWindowHeight / 2, 0));
+
+            World =
+                    // Translate to the center of the camera bounds.
+                    Matrix.CreateTranslation(cameraCenter) *
+                    // Rotate the camera relative to the center of the camera bounds.
+                    Matrix.CreateRotationZ(rotation);
+
+            View = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Up);
+
             Projection = Matrix.CreateOrthographic(ScreenManager.CurrentWindowWidth / Zoom, ScreenManager.CurrentWindowHeight / Zoom, -1000, 1000);
         }
     }
